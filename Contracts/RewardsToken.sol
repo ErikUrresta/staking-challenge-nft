4570ecd8865ae47494dc31a9babc1a6266931c2b
Erik Urresta Romero <48209014+ErikUrresta@users.noreply.github.com>
2022-10-03 17:42:22 +0200

    ERC20

--- Contracts/RewardsToken.sol
+++ Contracts/RewardsToken.sol
@@ -1,19 +1,34 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity 0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 
-contract RewardsToken is ERC20, Ownable {
-    constructor() ERC20("RewardsToken", "RTEU") {
-        _mint(msg.sender, 1000000 * 10 ** decimals());
-    }
+contract RewardsToken is ERC20, ERC20Burnable, Ownable {
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
+  mapping(address => bool) controllers;
+  
+  constructor() ERC20("N2DRewards", "N2DR") { }
+
+  function mint(address to, uint256 amount) external {
+    require(controllers[msg.sender], "Only controllers can mint");
+    _mint(to, amount);
+  }
+
+  function burnFrom(address account, uint256 amount) public override {
+      if (controllers[msg.sender]) {
+          _burn(account, amount);
+      }
+      else {
+          super.burnFrom(account, amount);
+      }
+  }
+
+  function addController(address controller) external onlyOwner {
+    controllers[controller] = true;
+  }
+
+  function removeController(address controller) external onlyOwner {
+    controllers[controller] = false;
+  }
 }
-
-
-
